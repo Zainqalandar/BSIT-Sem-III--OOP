04fd80212e358a42f2debb20d208454d7e690ca7
Zainqalandar <zainqlandar@gmail.com>
2025-01-03 21:07:22 +0500

    The folders for Tayab and Umar added in BSIT-Sem(iii)-OOP Repo

---
+++ Tayab-tahir/Addition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int value1, value2, sum;
+
+    value1 = 45;
+    value2 = 75;
+
+    sum = value1 + value2;
+
+    cout << "The sum of Value1 and Value2 is: " << sum <<endl;
+
+    return 0;
+}
